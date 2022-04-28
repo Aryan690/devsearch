09f47c3d9f9212a471fbb819da3b1b70f962d6fb
Aryan Setia <aryansetiaa@gmail.com>
2022-04-28 10:38:15 +0530

    initial

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,7 @@
+from dataclasses import field
+from tkinter import Widget
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -6,3 +9,13 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        for name,field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+        # self.fields['title'].widget.attrs.update({'class':'input','placeholder':'Add Title'})
+       
--- projects/models.py
+++ projects/models.py
@@ -1,12 +1,13 @@
 from email.policy import default
-from uuid import uuid4
+import uuid
 from django.db import models
 from django.forms import UUIDField
-
+from users.models import Profile
 # Create your models here.
 
 
 class Project(models.Model):
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)  # not required field
     featured_image = models.ImageField(null=True, blank=True, default = 'default.jpg')
@@ -16,7 +17,7 @@ class Project(models.Model):
     vote_total = models.IntegerField(default=0, null=True, blank=True)
     vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid4, unique=True,
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
 
     def __str__(self) -> str:
@@ -33,7 +34,7 @@ class Review(models.Model):
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=200, choices=VOTE_TYPE)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid4, unique=True,
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
 
     def __str__(self):
@@ -43,7 +44,7 @@ class Review(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid4, unique=True,
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
 
     def __str__(self):
