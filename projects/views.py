2377f3a8449dc754f59b86902dad20e87514c409
Aryan Setia <aryansetiaa@gmail.com>
2022-04-29 23:54:44 +0530

    added the user registration page

--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -16,7 +17,7 @@ def project(request, pk):
     print('projectObj:', projectObj)
     return render(request, 'projects/single-project.html', {'project': projectObj})
 
-
+@login_required(login_url='login')
 def createProject(request):
     form = ProjectForm()
 
@@ -29,7 +30,7 @@ def createProject(request):
     context = {'form':form}
     return render(request, 'projects/project_forms.html', context)
 
-
+@login_required(login_url='login')
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
@@ -43,7 +44,7 @@ def updateProject(request, pk):
     context = {'form':form}
     return render(request, 'projects/project_forms.html', context)
 
-
+@login_required(login_url='login')
 def deleteProject(request,pk):
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
