7232cdd9351663c38c331128deabaa7ab1ef0d8c
Aryan Setia <aryansetiaa@gmail.com>
2022-05-15 13:40:47 +0530

    initial

--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Profile
+from django.core.mail import send_mail
+from django.conf import settings
+
+
 
 # @receiver(post_save, sender=Profile)
 def createProfile(sender, instance, created, **kwargs):
@@ -25,6 +29,17 @@ def updateUser(sender, instance, created, **kwargs):
         user.username = profile.username
         user.email = profile.email
         user.save()
+
+    subject = 'Welcome to DevSearch'
+    message = 'We are glad you are here!'
+
+    send_mail(
+        subject,
+        message,
+        settings.EMAIL_HOST_USER,
+        [profile.email],
+        fail_silently=False
+    )
 
 
 post_save.connect(createProfile, sender=User)
--- users/urls.py
+++ users/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('update-skill/<str:pk>/', views.updateSkill, name='update-skill'),
     path('delete-skill/<str:pk>/', views.deleteSkill, name='delete-skill'),
     path('inbox/', views.inbox, name='inbox'),
-    path('message/<str:pk>', views.viewMessage, name='message'),
+    path('message/<str:pk>/', views.viewMessage, name='message'),
+    path('create-message/<str:pk>/', views.createMessage, name='create-message'),
 
     path('', views.profiles, name='profiles'),
     path('profile/<str:pk>/', views.userProfile, name='user-profile'),
